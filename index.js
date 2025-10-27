32b5d1f648f12223cc51c172bd4138e17b5b6998
Fu56 <fuadabdela95@gmail.com>
2025-10-27 18:16:09 +0300

    connect the contact form to spread sheet

--- index.js
+++ index.js
@@ -18,3 +18,20 @@
         function closemenu(){
            sidemenu.style.right = "-200px";
         }
+
+          const scriptURL = '<https://script.google.com/macros/s/AKfycbzmNGk_iTQHZdo5JwNe1lDSSGfN4vKyXi68-vd_xGv9p86emv5QvZOMAHl9JeyU2vxWNg/exec>'
+  const form = document.forms['submit-to-google-sheet']
+  const msg= document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+            msg.innerHTML="Message sent successfully";
+            setTimeout(function(){
+                msg.innerHTML="";
+            },5000);
+            form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
